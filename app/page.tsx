4fd6825f573d80456db24b568ace6a2d3f2f3049
alokMIPL <alok@madhulata.com>
2026-08-24 09:28:18 +0530

    code updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import Samoa from "@/components/Samoa";
-import Samoa2 from "@/components/Samoa2";
 import SamoaMap from "@/components/SamoaMap";
 import Image from "next/image";
 
 export default function Home() {
   return <>
-  {/* <SamoaMap /> */}
-  {/* <Samoa /> */}
-  <Samoa2 />
+  <SamoaMap />
   </>;
 }
